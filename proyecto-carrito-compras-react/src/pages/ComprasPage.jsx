221fbd147263d342157770c888185795c3dda3ce
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-06 12:26:08 -0300

    Feat: Obtener productos de la API
    Implementa la obtención de productos desde la API
    utilizando useEffect para la carga inicial del componente.
    Muestra los productos obtenidos en consola.

--- proyecto-carrito-compras-react/src/pages/ComprasPage.jsx
+++ proyecto-carrito-compras-react/src/pages/ComprasPage.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export const ComprasPage = () => {
+    const [productos, setProductos] = useState([])
+    
+    const fetchProductos = async () => {
+        const response = await fetch('https://fakestoreapi.com/products')
+        const data = await response.json()
+        setProductos(data)
+        console.log(data);
+        
+    }
+
+    //Cuando carga el componente ejecuta una sola vez useEffect
+    //y obtiene los productos
+    useEffect(() => {
+      fetchProductos()
+          
+    }, [])
+    
   return (
     <div>ComprasPage</div>
   )
