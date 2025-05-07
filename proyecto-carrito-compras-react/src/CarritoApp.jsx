fcad2d5b6fcbb22219e58d621886f8254a73a647
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-07 09:09:13 -0300

    Agrega proveedor de productos al contexto.
    Crea un proveedor de contexto para administrar la lista de productos.

--- proyecto-carrito-compras-react/src/CarritoApp.jsx
+++ proyecto-carrito-compras-react/src/CarritoApp.jsx
@@ -3,16 +3,19 @@ import { Routes, Route, NavLink } from 'react-router-dom'
 import { NavBar } from './components/NavBar'
 import { ComprasPage } from './pages/ComprasPage'
 import { CarritoPage } from './pages/CarritoPage'
+import { ProductosProvider } from './context/ProductosProvider'
 
 export const CarritoApp = () => {
     return (
         <>
-            <NavBar></NavBar>
-            <Routes>
-                <Route path='/' element={<ComprasPage/>}/>
-                <Route path='/carrito' element={<CarritoPage/>}/>
-                <Route path='/*' element={<NavLink to='/'/>}/>
-            </Routes>
+            <ProductosProvider>
+                <NavBar></NavBar>
+                <Routes>
+                    <Route path='/' element={<ComprasPage />} />
+                    <Route path='/carrito' element={<CarritoPage />} />
+                    <Route path='/*' element={<NavLink to='/' />} />
+                </Routes>
+            </ProductosProvider>
         </>
     )
 }
---
+++ proyecto-carrito-compras-react/src/context/ProductosProvider.jsx
@@ -0,0 +1,24 @@
+import { ProductosContext } from "./ProductosContext"
+import { useState, useEffect } from 'react'
+
+
+export const ProductosProvider = ({ children }) => {
+    const [productos, setProductos] = useState([])
+
+    const fetchProductos = async () => {
+        const response = await fetch('https://fakestoreapi.com/products')
+        const data = await response.json()
+        setProductos(data)
+        console.log(data);
+    }
+    
+    useEffect(() => {
+        fetchProductos()
+
+    }, [])
+    return (
+        <ProductosContext.Provider value={{ productos, setProductos }}>
+            {children}
+        </ProductosContext.Provider>
+    )
+}
