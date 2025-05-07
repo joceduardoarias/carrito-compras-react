ccb94db3bbbddb4dc2b7b508236c58a7056aa3dc
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-07 08:33:11 -0300

    Agrega tabla básica de carrito de compras
    Implementa una estructura HTML básica para la página del carrito, incluyendo una tabla para mostrar los productos y un botón de compra.

--- proyecto-carrito-compras-react/src/pages/CarritoPage.jsx
+++ proyecto-carrito-compras-react/src/pages/CarritoPage.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export const CarritoPage = () => {
   return (
-    <div>CarritoPage</div>
+    <>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col">Eliminar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>        
+        </tbody>
+      </table>
+      <div className='d-grid gap-2'>
+        <button className='btn btn-primary'> Comprar</button>
+      </div>
+    </>
   )
 }
