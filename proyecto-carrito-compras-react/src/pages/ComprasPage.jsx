fa977612f4d56f05cdbb78b18097454552c39f4d
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-07 09:10:03 -0300

    Refactor: Simplifica obtención de productos
    Reemplaza la obtención de productos desde la API directamente en el componente por el uso del contexto de Productos.

--- proyecto-carrito-compras-react/src/pages/ComprasPage.jsx
+++ proyecto-carrito-compras-react/src/pages/ComprasPage.jsx
@@ -1,23 +1,10 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { Card } from '../components/Card'
+import { ProductosContext } from '../context/ProductosContext'
 
 export const ComprasPage = () => {
-    const [productos, setProductos] = useState([])
 
-    const fetchProductos = async () => {
-        const response = await fetch('https://fakestoreapi.com/products')
-        const data = await response.json()
-        setProductos(data)
-        console.log(data);        
-    }
-
-    //Cuando carga el componente ejecuta una sola vez useEffect
-    //y obtiene los productos
-    useEffect(() => {
-        fetchProductos()
-
-    }, [])
+    const {productos} = useContext(ProductosContext)
 
     return (
         <>
