f02ba903ab404e77b18c9eb130e2a6be52795ba3
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-09 13:16:27 -0300

    Refactoriza: Renombra y actualiza rutas de productos
    Renombra el archivo de rutas de productos para mayor claridad y consistencia.
    
    Actualiza la ruta base para productos a '/api/productos' para alinearse con la estructura de la API.
    
    Exporta la variable 'route' para ser utilizada en otras partes de la aplicación.

--- backend-api/src/routes/productosRoutes.js
+++ backend-api/src/routes/productosRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import productosController from '../controllers/productos'
+import productosController from '../controllers/productosController.js'
 
 const route = express.Router();
 
 
-route.get('/', productosController.getAll);
+route.get('/api/productos', productosController.getAll);
 route.get('/:id', productosController.getById);
 route.post('/', productosController.create);
 route.put('/:id', productosController.update);
 route.delete('/:id', productosController.delete);
+
+export default route;
