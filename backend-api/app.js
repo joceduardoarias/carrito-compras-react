a4a7c4bf6db7b18de2aa40a4edc2d5bc1992f23a
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-09 13:16:48 -0300

    Feat: Configurar rutas y servidor Express
    Configura las rutas de la API utilizando Express y define el puerto del servidor.
    
    Muestra un mensaje en la consola al iniciar el servidor, indicando la URL de acceso.

--- backend-api/app.js
+++ backend-api/app.js
@@ -1,12 +1,18 @@
 import express from "express";
 import 'dotenv/config';
+import productosRoutes from './src/routes/productosRoutes.js';
+
 const app = express();
+
+// Definir rutas
+app.use('/', productosRoutes);
 
 try {
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT,()=> console.log('Servidor activo en el puerto ' + PORT)
-    )
+            
+    app.listen(PORT, () => {
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
 } catch (e) {
     console.log(e);
-    
 }
