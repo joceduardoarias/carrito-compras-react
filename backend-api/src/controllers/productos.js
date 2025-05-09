ddcda00678bc07052875dc35f961c297a2d1435e
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-09 12:40:55 -0300

    Feat: Inicializar controlador de productos
    Crea el controlador de productos con métodos básicos
    para las operaciones CRUD (Crear, Leer, Actualizar, Eliminar),
    estableciendo la estructura inicial para la gestión de productos
    en la API.

---
+++ backend-api/src/controllers/productos.js
@@ -0,0 +1,43 @@
+class productosController {
+    constructor(){
+
+    }
+
+    async create (req, res) {
+        try {
+            
+        } catch (e) {
+            
+        }
+    }
+    async getAll (req, res) {
+        try {
+            
+        } catch (e) {
+            
+        }
+    }
+    async getById (req, res) {
+        try {
+            
+        } catch (e) {
+            
+        }
+    }
+    async update (req, res) {
+        try {
+            
+        } catch (e) {
+            
+        }
+    }
+    async delete (req, res) {
+        try {
+            
+        } catch (e) {
+            
+        }
+    }
+}
+
+export default new productosController();
