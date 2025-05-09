aee4a4f5b2b28c6404ead58c8e95d6e2deb8f61d
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-09 12:24:29 -0300

    Feat: Inicializar servidor Express con dotenv
    Implementa la configuración básica del servidor
    Express utilizando la biblioteca dotenv para
    gestionar variables de entorno.  Permite una
    configuración flexible del puerto a través de
    variables de entorno, facilitando el despliegue
    en diferentes entornos.

---
+++ backend-api/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import 'dotenv/config';
+const app = express();
+
+try {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT,()=> console.log('Servidor activo en el puerto ' + PORT)
+    )
+} catch (e) {
+    console.log(e);
+    
+}
