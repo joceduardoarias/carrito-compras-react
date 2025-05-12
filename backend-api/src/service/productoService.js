fffe3b71dd54e4c580a75b050aa25eca9c38ad32
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-12 12:32:11 -0300

    Refactor: Refactoriza métodos del servicio de productos
    Refactoriza los métodos del servicio de productos para mejorar la gestión de errores y la búsqueda de productos.
    
    - Modifica la función `getProductById` para lanzar un error si el producto no se encuentra.
    - Actualiza la función `updateProduct` para buscar y actualizar un producto por ID, lanzando un error si no se encuentra.
    - Modifica la función `deleteProduct` para buscar y eliminar un producto por ID, lanzando un error si no se encuentra.

--- backend-api/src/service/productoService.js
+++ backend-api/src/service/productoService.js
@@ -1,12 +1,10 @@
 import Producto from "../models/producto.js";
 
 class ProductoService {
-    constructor() {
-        
-    }
+    constructor() {}
 
-    async  createProduct (data) {
-        if (!data.title || !data.description || !data.price || !data.image){
+    async createProduct(data) {
+        if (!data.title || !data.description || !data.price || !data.image) {
             throw new Error('Todos los campos son requeridos');
         }
         try {
@@ -17,7 +15,7 @@ class ProductoService {
         }
     }
 
-    async getAllProducts () {
+    async getAllProducts() {
         try {
             const productos = await Producto.find();
             return productos;
@@ -26,30 +24,39 @@ class ProductoService {
         }
     }
 
-    async getProductById (id){
+    async getProductById(id) {
         try {
-            const producto = Producto.getById(id);
-            return producto;
-        } catch (error) {
-            throw error
-        }
-    }
-
-    async updateProduct (data){
-        if (!data.title || !data.description || !data.price || !data.image){
-            throw new Error('Todos los campos son requeridos');
-        }
-        try {
-            const producto = await Producto.update(data);
+            const producto = await Producto.findById(id);
+            if (!producto) {
+                throw new Error('Producto no encontrado');
+            }
             return producto;
         } catch (error) {
             throw error;
         }
     }
 
-    async deleteProduct (id){
+    async updateProduct(id, data) {
+        if (!data.title || !data.description || !data.price || !data.image) {
+            throw new Error('Todos los campos son requeridos');
+        }
         try {
-            const deleted = await Producto.remove(id);
+            const producto = await Producto.findByIdAndUpdate(id, data, { new: true });
+            if (!producto) {
+                throw new Error('Producto no encontrado');
+            }
+            return producto;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteProduct(id) {
+        try {
+            const deleted = await Producto.findByIdAndDelete(id);
+            if (!deleted) {
+                throw new Error('Producto no encontrado');
+            }
             return deleted;
         } catch (error) {
             throw error;
