200f00e1c9856515e7a2460224c22bcaba972a7c
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-12 14:54:48 -0300

    Implementa servicio de usuario con login y registro
    Agrega la funcionalidad de login y registro de usuarios.
    Incluye el hash de contraseñas para mayor seguridad.

--- backend-api/src/service/usuarioService.js
+++ backend-api/src/service/usuarioService.js
@@ -1,21 +1,45 @@
 import Usuario from '../models/usuario.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 class UsuarioService {
     async login(email, password) {
         try {
-            
             const usuario = await Usuario.findOne({ email });
             if (!usuario) {
                 throw new Error('Usuario no encontrado');
             }
 
-            
-            const isPasswordValid = await bcrypt.compare(password, usuario.password);
-            if (!isPasswordValid) {
+            const contraseñaValida = await bcrypt.compare(password, usuario.password);
+            if (!contraseñaValida) {
                 throw new Error('Contraseña incorrecta');
             }
 
+            return usuario;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async register(data) {
+        try {
+            const { email, password, role } = data;
+            
+            const usuarioRegistrado = await Usuario.findOne({ email });
+            if (usuarioRegistrado) {
+                throw new Error('El usuario ya está registrado');
+            }
+
+            
+            const hasPassword = await bcrypt.hash(password, 10);
+
+            
+            const usuario = new Usuario({
+                email,
+                password: hasPassword,
+                role: role || 'comun',
+            });
+
+            await usuario.save();
             return usuario;
         } catch (error) {
             throw error;
