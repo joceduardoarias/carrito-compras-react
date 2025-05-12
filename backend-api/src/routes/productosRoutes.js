628a4ea0860ab6b470d96c4a08c0bcf513032487
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-12 12:38:50 -0300

    Fix: Corrige la ruta PUT de producto por ID
    Corrige la ruta PUT en el controlador de productos para que coincida con el formato correcto de la ruta con el ID del producto.
    
    Anteriormente, la ruta PUT tenía un formato incorrecto (`/api/producto:id`), lo que impedía que la solicitud se dirigiera correctamente al controlador.
    
    La modificación cambia la ruta a `/api/producto/:id` para garantizar que el ID del producto se capture correctamente como un parámetro de ruta.

--- backend-api/src/routes/productosRoutes.js
+++ backend-api/src/routes/productosRoutes.js
@@ -7,7 +7,7 @@ const route = express.Router();
 route.get('/api/productos', productosController.getAll);
 route.get('/api/producto/:id', productosController.getById);
 route.post('/api/producto', productosController.create);
-route.put('/api/producto:id', productosController.update);
+route.put('/api/producto/:id', productosController.update);
 route.delete('/api/producto/:id', productosController.delete);
 
 export default route;
