fe024e3c7489039e6b068b661687ac83269f8494
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-13 10:35:32 -0300

    Feat: Implementar autenticación de usuarios
    Añade la funcionalidad de autenticación de usuarios al proyecto.
    Protege las rutas de la aplicación para requerir autenticación antes de acceder a ellas.
    Utiliza el contexto de UsuariosProvider para gestionar el estado de autenticación.

--- proyecto-carrito-compras-react/src/ProductosApp.jsx
+++ proyecto-carrito-compras-react/src/ProductosApp.jsx
@@ -3,20 +3,31 @@ import { Routes, Route, NavLink } from 'react-router-dom'
 import { NavBar } from './components/NavBar'
 import { ProductosPage } from './pages/ProductosPage'
 import { ProductosProvider } from './context/ProductosProvider'
-import {EditarProductoPage} from './pages/EditarProductoPage'
+import { EditarProductoPage } from './pages/EditarProductoPage'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { UsuariosProvider } from './context/UsuariosProvider'
+import { LoginPage } from "../src/pages/LoginPage"
+import Protected from '../../../../React/book-store/src/protected/Protected'
+
 
 export const ProductosApp = () => {
     return (
-        <>
+
+
+        <UsuariosProvider>
             <ProductosProvider>
                 <NavBar></NavBar>
                 <Routes>
-                    <Route path='/' element={<ProductosPage />} /> 
-                    <Route path="/editar-producto" element={<EditarProductoPage />} />                   
-                    <Route path='/*' element={<NavLink to='/' />} />
+                    <Route path='login' element={<LoginPage />} />
+                    <Route element={<Protected />}>
+                        <Route path='/' element={<ProductosPage />} />
+                        <Route path="/editar-producto" element={<EditarProductoPage />} />
+                        <Route path='/*' element={<NavLink to='/' />} />
+                    </Route>
                 </Routes>
             </ProductosProvider>
-        </>
+        </UsuariosProvider>
+
+
     )
 }
